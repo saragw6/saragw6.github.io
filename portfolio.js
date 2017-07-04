98197b9c5d29b4cbfeec4e6a42ec45b6835a5d5c
Sara <saragw6@gmail.com>
2017-07-04 03:52:02 -0700

    svg scroll & link regions, page color neutral

--- portfolio.js
+++ portfolio.js
@@ -1,41 +1,55 @@
 $(document).ready(function(){
 // Add smooth scrolling to all links
-  $(".caret, .sellpt > a").on('click', function(event) {
-    //alert('this.href= ' + this.href);
+  $(".caret, svg > a").on('click', function(event) {
+    console.log(this.href.baseVal);
     // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
+    if (!$.isEmptyObject(this.href.baseVal)) {
+      smoothScroll(this.href.baseVal);
+    } else if (!$.isEmptyObject(this.hash)) {
       // Prevent default anchor click behavior
-      event.preventDefault();
 
-      // Store hash
-      var hash = this.hash;
+      smoothScroll(this.hash);
 
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 400, function(){
-   
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
     } // End if
   });
 });
 
+function smoothScroll(target) {
+    event.preventDefault();
+    var hash = target;
+    console.log($(hash));
+
+    // Using jQuery's animate() method to add smooth page scroll
+    // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 400, function(){
+
+      // Add hash (#) to URL when done scrolling (default click behavior)
+      window.location.hash = hash;
+
+  });
+}
+
 //SCROLL ANIMATE SVG
 $(document).ready(function() {
   //variable for the 'stroke-dashoffset' unit
-  console.log($(".clipper").css("height"));
+  updateSidebar();
   //on a scroll event - execute function
   $(window).scroll(function() {
-    //calculate how far down the page the user is 
-    var $percentageComplete = (($(window).scrollTop() / ($("html").height() - $(window).height())));
-    //convert dashoffset pixel value to interger
-    //var $newUnit = parseInt($dashOffset, 10);
-    //get the value to be subtracted from the 'stroke-dashoffset'
-    var $newHeight = $percentageComplete * 640;
-    //set the new value of the dashoffset to create the drawing effect
-    $(".clipper").css("height", $newHeight);
+    updateSidebar();
   });
 });
+
+function updateSidebar() {
+    //calculate how far down the page the user is 
+    var $percentageComplete = (($(window).scrollTop() / ($("html").height() - $(window).height())));
+    //get the value to be subtracted from the 'stroke-dashoffset'
+    var $newHeight = $percentageComplete * 650;
+    //set the new value of the dashoffset to create the drawing effect
+    $(".clipper").css("height", $newHeight);
+}
+
+function contains(str, substr) {
+  return ~str.indexOf(substr);
+}
