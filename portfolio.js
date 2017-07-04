0af8f05f00d46981ebe756cd512d568c8d3e27c3
Sara <saragw6@gmail.com>
2017-07-03 22:10:57 -0700

    added links to header icons

--- portfolio.js
+++ portfolio.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
   // Add smooth scrolling to all links
-  $(".caret").on('click', function(event) {
+  $(".caret, .sellpt > a").on('click', function(event) {
+    //alert('this.href= ' + this.href);
     // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
       // Prevent default anchor click behavior
@@ -13,7 +14,7 @@ $(document).ready(function(){
       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
         scrollTop: $(hash).offset().top
-      }, 800, function(){
+      }, 400, function(){
    
         // Add hash (#) to URL when done scrolling (default click behavior)
         window.location.hash = hash;
