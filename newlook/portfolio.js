71f8f724f46f908f013e14df1cd6b41a74600b67
Sara <saragw6@gmail.com>
2017-08-17 22:41:39 -0700

    clickable name and navbar link home

--- newlook/portfolio.js
+++ newlook/portfolio.js
@@ -2,7 +2,7 @@ $(document).ready(addSmoothScroll);
 
 function addSmoothScroll(){
 // Add smooth scrolling to all links
-  $(".angle-down-wrap > a, svg > a, #navbaricons > a, #heroicons > a").on('click', function(event) {
+  $(".angle-down-wrap > a, svg > a, #navbaricons > a, #heroicons > a, #navbarname > a").on('click', function(event) {
     console.log("baseval ", this.href.baseVal);
     // Make sure this.hash has a value before overriding default behavior
     if (!$.isEmptyObject(this.href.baseVal)) {
@@ -19,11 +19,11 @@ function addSmoothScroll(){
 //change name
 $(document).ready(function() {
 
-  $("#navbarname").html(getNavBarName());
+  $("#navbarname > a").html(getNavBarName());
 
   //on a scroll event - execute function
   $(window).resize(function() {
-    $("#navbarname").html(getNavBarName());
+    $("#navbarname > a").html(getNavBarName());
   });
 
 });
@@ -51,66 +51,72 @@ function getNavBarName() {
   return $(window).height() >= (55/67) * $(window).width() - (325/67) ? "SARA GW" : "SARA GOLDSTEIN-WEISS";
 }
 
-//SCROLL ANIMATE SVG
-/*$(document).ready(function() {
-  //variable for the 'stroke-dashoffset' unit
-  updateSvgWidth();
-  updateSidebarFill();
-  //on a scroll event - execute function
-  $(window).scroll(function() {
+
+/////// SVG SIDEBAR FUNCTIONS ///////////////
+
+  //SCROLL ANIMATE SVG
+  /*$(document).ready(function() {
+    //variable for the 'stroke-dashoffset' unit
+    updateSvgWidth();
     updateSidebarFill();
-  });
+    //on a scroll event - execute function
+    $(window).scroll(function() {
+      updateSidebarFill();
+    });
 
-  $(window).resize(function() {
-    updateSvgWidth()
-  });
-});*/
-
-
-/*function updateSvgWidth() {
-  var $maxWidth = Math.min(200, $(window).height() * .25);
-  var $newWidth = $("#sidebar").width();
-
-  $("#sidebar > svg").css("width", Math.min($newWidth, $maxWidth));
-}
-*/
-
-/*function updateSidebarFill() {
-    //calculate how far down the page the user is 
-    var $percentageComplete = (($(window).scrollTop() / ($("html").height() - $(window).height())));
-    //get the value to be subtracted from the 'stroke-dashoffset'
-    var $newHeight = $percentageComplete * 650;
-    //set the new value of the dashoffset to create the drawing effect
-    $(".clipper").css("height", $newHeight);
-}*/
-
-// function contains(str, substr) {
-//   return ~str.indexOf(substr);
-// }
+    $(window).resize(function() {
+      updateSvgWidth()
+    });
+  });*/
 
 
-// //scroll svg draw cmopuer
-// $(document).ready(function() {
-//   $(".filled").css("opacity", "0");
+  /*function updateSvgWidth() {
+    var $maxWidth = Math.min(200, $(window).height() * .25);
+    var $newWidth = $("#sidebar").width();
 
-//   //variable for the 'stroke-dashoffset' unit
-//   var $dashOffset = $(".path").css("stroke-dashoffset");
-//   //on a scroll event - execute function
-//   $(window).scroll(function() {
-//     //calculate how far down the page the user is 
-//     var $percentageComplete = (($(window).scrollTop() / ($("html").height() - $(window).height())) * 100);
+    $("#sidebar > svg").css("width", Math.min($newWidth, $maxWidth));
+  }
+  */
 
-//     //convert dashoffset pixel value to interger
-//     var $newUnit = parseInt($dashOffset, 10);
-//     console.log("svg scroll", $percentageComplete * ($newUnit / 67));
+  /*function updateSidebarFill() {
+      //calculate how far down the page the user is 
+      var $percentageComplete = (($(window).scrollTop() / ($("html").height() - $(window).height())));
+      //get the value to be subtracted from the 'stroke-dashoffset'
+      var $newHeight = $percentageComplete * 650;
+      //set the new value of the dashoffset to create the drawing effect
+      $(".clipper").css("height", $newHeight);
+  }*/
 
-//     //get the value to be subtracted from the 'stroke-dashoffset'
-//     var $offsetUnit = $percentageComplete * ($newUnit / 67);
-//     //set the new value of the dashoffset to create the drawing effect
-//     $(".path").css("stroke-dashoffset", $newUnit - $offsetUnit);
+  // function contains(str, substr) {
+  //   return ~str.indexOf(substr);
+  // }
 
-//   });
-// });
+
+  // //scroll svg draw cmopuer
+  // $(document).ready(function() {
+  //   $(".filled").css("opacity", "0");
+
+  //   //variable for the 'stroke-dashoffset' unit
+  //   var $dashOffset = $(".path").css("stroke-dashoffset");
+  //   //on a scroll event - execute function
+  //   $(window).scroll(function() {
+  //     //calculate how far down the page the user is 
+  //     var $percentageComplete = (($(window).scrollTop() / ($("html").height() - $(window).height())) * 100);
+
+  //     //convert dashoffset pixel value to interger
+  //     var $newUnit = parseInt($dashOffset, 10);
+  //     console.log("svg scroll", $percentageComplete * ($newUnit / 67));
+
+  //     //get the value to be subtracted from the 'stroke-dashoffset'
+  //     var $offsetUnit = $percentageComplete * ($newUnit / 67);
+  //     //set the new value of the dashoffset to create the drawing effect
+  //     $(".path").css("stroke-dashoffset", $newUnit - $offsetUnit);
+
+  //   });
+  // });
+///////////////////////////////////////////////
+
+
 
 // header & hero transition
 ////use scrolltop and transitions and toggle classes to change height/display of header and opacity of hero content
